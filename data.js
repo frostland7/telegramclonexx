ca210c46862e70e1d8d74595475ff9d78f163fc8
frostland7 <cebanvitalii45@gmail.com>
2026-06-21 16:05:50 +0300

    Jvh
    No fh

---
+++ data.js
@@ -0,0 +1,10 @@
+const DB = {
+    chats: [
+        { id: 1, name: "Павел Дуров", last: "Код готов?" },
+        { id: 2, name: "Команда Telegram", last: "Обновление v.4.0" }
+    ],
+    msgs: {
+        1: [{ text: "Привет! Как успехи?", from: "other", time: "14:20" }],
+        2: [{ text: "Обновление готово", from: "other", time: "10:00" }]
+    }
+};
